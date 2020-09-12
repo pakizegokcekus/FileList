e15dd8719d6e4e5e690be2b6ca82f8fe7abe6a9d
pakizekus <48360370+pakizekus@users.noreply.github.com>
2020-09-13 00:32:51 +0300

    ExportToExcel Add

--- FileList/Form1.Designer.cs
+++ FileList/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.txtFilePath = new System.Windows.Forms.TextBox();
             this.pnlTop = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExportExcel = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.btnWatch = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
@@ -82,27 +83,39 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnExportExcel);
             this.panel1.Controls.Add(this.listBox1);
             this.panel1.Controls.Add(this.btnWatch);
             this.panel1.Controls.Add(this.listView1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 54);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(820, 379);
+            this.panel1.Size = new System.Drawing.Size(820, 393);
             this.panel1.TabIndex = 4;
+            // 
+            // btnExportExcel
+            // 
+            this.btnExportExcel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExportExcel.Location = new System.Drawing.Point(617, 283);
+            this.btnExportExcel.Name = "btnExportExcel";
+            this.btnExportExcel.Size = new System.Drawing.Size(93, 23);
+            this.btnExportExcel.TabIndex = 4;
+            this.btnExportExcel.Text = "Exele Aktar";
+            this.btnExportExcel.UseVisualStyleBackColor = true;
+            this.btnExportExcel.Click += new System.EventHandler(this.btnExportExcel_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(163, 280);
+            this.listBox1.Location = new System.Drawing.Point(12, 283);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(281, 95);
+            this.listBox1.Size = new System.Drawing.Size(377, 95);
             this.listBox1.TabIndex = 3;
             // 
             // btnWatch
             // 
             this.btnWatch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnWatch.Location = new System.Drawing.Point(688, 283);
+            this.btnWatch.Location = new System.Drawing.Point(716, 283);
             this.btnWatch.Name = "btnWatch";
             this.btnWatch.Size = new System.Drawing.Size(93, 23);
             this.btnWatch.TabIndex = 2;
@@ -170,13 +183,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(820, 433);
+            this.ClientSize = new System.Drawing.Size(820, 447);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pnlTop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "s";
             this.pnlTop.ResumeLayout(false);
             this.pnlTop.PerformLayout();
             this.panel1.ResumeLayout(false);
@@ -200,8 +212,9 @@
         private System.Windows.Forms.ColumnHeader clmnUpdateTime;
         private System.IO.FileSystemWatcher fileSystemWatcher1;
         private System.Windows.Forms.Button btnWatch;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Timer timerChange;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnExportExcel;
     }
 }
 
--- FileList/Form1.cs
+++ FileList/Form1.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Color = System.Drawing.Color;
+using Control = System.Windows.Forms.Control;
+using ListViewItem = System.Windows.Forms.ListViewItem;
 
 
 namespace FileList
@@ -21,7 +25,26 @@ namespace FileList
             this.listView1.CheckBoxes = true;
             Control.CheckForIllegalCrossThreadCalls = false;
         }
+
+        private bool isMatch = false;
+        #region Filelisting
+
         bool ctr = false;
+
+        private void btnFilePath_Click(object sender, EventArgs e)
+        {
+            
+            listView1.Items.Clear();
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = folderBrowserDialog1.SelectedPath.ToString();
+                isMatch = true;
+            }
+
+            DirSearch_ex3(txtFilePath.Text);
+
+        }
 
         void DirSearch_ex3(string sDir)
         {
@@ -33,16 +56,12 @@ namespace FileList
                 foreach (string f in Directory.GetFiles(sDir))
                 {
 
-
                     FileInfo dosyabilgisi = new FileInfo(f);
                     ListViewItem ekle = listView1.Items.Add(dosyabilgisi.FullName);
                     ekle.SubItems.Add(dosyabilgisi.Name);
                     ekle.SubItems.Add(dosyabilgisi.Length.ToString() + " Byte");
                     ekle.SubItems.Add(dosyabilgisi.CreationTime.ToString());
                     ekle.SubItems.Add(dosyabilgisi.LastWriteTimeUtc.ToString());
-
-
-
 
                 }
 
@@ -60,39 +79,42 @@ namespace FileList
         }
 
 
-        private void btnFilePath_Click(object sender, EventArgs e)
-        {
-            listView1.Items.Clear();
+       
 
-            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
-            {
-                txtFilePath.Text = folderBrowserDialog1.SelectedPath.ToString();
-            }
+        #endregion
 
-            DirSearch_ex3(txtFilePath.Text);
+        #region FileWatch
 
-        }
 
         private void btnWatch_Click(object sender, EventArgs e)
         {
-
-            FileSystemWatcher watcher = new FileSystemWatcher(txtFilePath.Text);
-
-            watcher.EnableRaisingEvents = true;
-            watcher.IncludeSubdirectories = true;
-
-            //add Event Hanlders
-            watcher.Changed += fileSystemWatcher1_Changed;
-            watcher.Created += fileSystemWatcher1_Created;
-            watcher.Deleted += fileSystemWatcher1_Deleted;
-            watcher.Renamed += fileSystemWatcher1_Renamed;
-
-            timerChange.Start();
            
+
+            try
+            {
+                FileSystemWatcher watcher = new FileSystemWatcher(txtFilePath.Text);
+
+                watcher.EnableRaisingEvents = true;
+                watcher.IncludeSubdirectories = true;
+
+                //add Event Hanlders
+                watcher.Changed += fileSystemWatcher1_Changed;
+                watcher.Created += fileSystemWatcher1_Created;
+                watcher.Deleted += fileSystemWatcher1_Deleted;
+                watcher.Renamed += fileSystemWatcher1_Renamed;
+
+                timerChange.Start();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+
 
         }
 
-        
+
         private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
         {
             listBox1.Items.Add(string.Format("Change: {0} {1}", e.FullPath, e.ChangeType));
@@ -102,7 +124,7 @@ namespace FileList
         private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
         {
             listBox1.Items.Add(string.Format("Create: {0} {1}", e.FullPath, e.ChangeType));
-           // listView1.CheckedItems[e.ToString()].BackColor = Color.Aqua;
+            // listView1.CheckedItems[e.ToString()].BackColor = Color.Aqua;
             ctr = true;
         }
 
@@ -118,29 +140,159 @@ namespace FileList
             ctr = true;
         }
 
-       
+
         int sayac = 0;
+
         private void timerChange_Tick(object sender, EventArgs e)
         {
-            sayac++;
-
-           
-
-            if (sayac % 2 == 0)
+            try
             {
+                sayac++;
 
-                foreach (ListViewItem item in listView1.CheckedItems)
+
+
+                if (sayac % 2 == 0)
                 {
-                    if (ctr == true)
-                    {
-                        this.listView1.Items[item.Index].BackColor = Color.Red;
-                    }
-                    ctr = false;
-                }
 
-                
+                    foreach (ListViewItem item in listView1.CheckedItems)
+                    {
+                        if (ctr == true)
+                        {
+                            this.listView1.Items[item.Index].BackColor = Color.Red;
+                        }
+
+                        ctr = false;
+                    }
+
+
+                }
+            }
+
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+                throw;
             }
 
         }
+
+        #endregion
+
+        #region ExporttoExcel
+
+       
+        private void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            
+           
+            if (isMatch == true)
+            {
+                ExportToExcel();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Önce Excele Aktarılacak Dosya Yolunu Seçiniz!", "Uyarı", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            
+        }
+
+        public void ExportToExcel()
+        {
+            try
+            {
+
+                using (SaveFileDialog sfd = new SaveFileDialog()
+                {
+                    Filter = "Excel|*.xlsx",
+                    ValidateNames = true,
+
+                })
+                {
+
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+
+
+                        SpreadsheetDocument xl = SpreadsheetDocument.Create(sfd.FileName, SpreadsheetDocumentType.Workbook);
+
+
+                        List<OpenXmlAttribute> oxa;
+                        OpenXmlWriter oxw;
+
+                        xl.AddWorkbookPart();
+                        WorksheetPart wsp = xl.WorkbookPart.AddNewPart<WorksheetPart>();
+
+                        oxw = OpenXmlWriter.Create(wsp);
+                        oxw.WriteStartElement(new Worksheet());
+                        oxw.WriteStartElement(new SheetData());
+
+
+                        foreach (ListViewItem item in listView1.Items)
+                        {
+
+                            oxa = new List<OpenXmlAttribute>();
+                            oxw.WriteStartElement(new Row(), oxa);
+
+
+
+                            foreach (ListViewItem.ListViewSubItem subItem in item.SubItems)
+                            {
+
+                                oxa = new List<OpenXmlAttribute>();
+                                oxw.WriteStartElement(new Cell(), oxa);
+
+
+                                oxw.WriteElement(new CellValue(subItem.ToString()));
+
+                                // this is for Cell
+                                oxw.WriteEndElement();
+
+
+                            }
+
+                            // this is for Row
+                            oxw.WriteEndElement();
+                        }
+
+                        // this is for SheetData
+                        oxw.WriteEndElement();
+                        // this is for Worksheet
+                        oxw.WriteEndElement();
+                        oxw.Close();
+
+                        oxw = OpenXmlWriter.Create(xl.WorkbookPart);
+                        oxw.WriteStartElement(new Workbook());
+                        oxw.WriteStartElement(new Sheets());
+
+                        oxw.WriteElement(new Sheet()
+                        {
+                            Name = "Sheet1",
+                            SheetId = 1,
+                            Id = xl.WorkbookPart.GetIdOfPart(wsp)
+                        });
+
+                        // this is for Sheets
+                        oxw.WriteEndElement();
+                        // this is for Workbook
+                        oxw.WriteEndElement();
+                        oxw.Close();
+
+                        xl.Close();
+                        MessageBox.Show("Veriler Excel Dosyasına Başarıyla Kaydedildi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+
+        #endregion
+
     }
 }
